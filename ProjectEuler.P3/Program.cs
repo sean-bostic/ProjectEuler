1b350fbad2a3ba7c0ea594c718f45dde9440a669
Sean Bostic <sean.bostic@gmail.com>
2017-04-04 22:02:28 -0400

    Problem 3 Solution
    Problem 3 Solution + Problem 4 start.

--- ProjectEuler.P3/Program.cs
+++ ProjectEuler.P3/Program.cs
@@ -6,40 +6,79 @@ using System.Threading.Tasks;
 
 namespace ProjectEuler.P3
 {
+    #region attempt1
+    //class Program
+    //{
+    //    //this currently is not working....need to figure this out
+    //    static void Main(string[] args)
+    //    {
+    //        //The prime factors of 13195 are 5, 7, 13 and 29.
+    //        //What is the largest prime factor of the number 600851475143?
+    //        long number = 600851475143;
+    //        long factorial = 0;
+
+    //        for (long i = 2; i < number; i++)
+    //        {
+    //            if (number % 1 == 0)
+    //            {
+    //                bool isPrimeNumber = true;
+    //                for (int j = 2; j < i; j++)
+    //                {
+    //                    if (i % j == 0)
+    //                    {
+    //                        isPrimeNumber = false;
+    //                        break;
+
+    //                    }
+    //                }
+    //                if (isPrimeNumber)
+    //                {
+    //                    factorial = i;
+    //                }
+    //            }
+    //        }
+
+    //        Console.WriteLine(factorial);
+    //        Console.Read();
+
+    //    }
+    //}
+    #endregion
+
     class Program
     {
-        //this currently is not working....need to figure this out
         static void Main(string[] args)
         {
-            //The prime factors of 13195 are 5, 7, 13 and 29.
-            //What is the largest prime factor of the number 600851475143?
-            long number = 600851475143;
-            long factorial = 0;
-
-            for (long i = 2; i < number; i++)
+            long NUM = 600851475143;
+            long LargestPrime = 0;
+            // NUM = (long)Math.Sqrt(NUM);
+            for (long i = 2; i * i < NUM; i++)
             {
-                if (number % 1 == 0)
-                {
-                    bool isPrimeNumber = true;
-                    for (int j = 2; j < i; j++)
+                if (IsFactorOf(NUM, i))
+                    if (IsPrime(i))
                     {
-                        if (i % j == 0)
-                        {
-                            isPrimeNumber = false;
-                            break;
-
-                        }
+                        Console.WriteLine(i + " : is prime & is factor ");
+                        if (i > LargestPrime)
+                            LargestPrime = i;
                     }
-                    if (isPrimeNumber)
-                    {
-                        factorial = i;
-                    }
-                }
             }
-
-            Console.WriteLine(factorial);
+            Console.WriteLine("\n Done.... and the Largest Prime Factor is : " + LargestPrime);
             Console.Read();
+        }
 
+        static bool IsPrime(long x)
+        {
+            for (int i = 2; i < x; ++i)
+                if (x % i == 0)
+                    return false;
+            return true;
+        }
+
+        static bool IsFactorOf(long x, long y)
+        {
+            if (x % y == 0)
+                return true;
+            return false;
         }
     }
 }
---
+++ ProjectEuler.P4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler.P4
+{
+    class Program
+    {
+        //A palindromic number reads the same both ways.The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+        //Find the largest palindrome made from the product of two 3-digit numbers.
+        static void Main(string[] args)
+        {
+            //bool isPalindrome = false;
+            //string number;
+
+            //if (number.Reverse() == number)
+            //{
+
+            //}
+        }
+    }
+}
