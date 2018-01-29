503cecdc12685e1aae28bc8c7fb64bcc99ca70b0
Sean Bostic <sean.bostic@gmail.com>
2018-01-29 00:04:34 -0500

    Adding problem folders
    Hoping to hold all the code in this folder and make calls to it through
    the interface.
    
    TODO:
    -Move existing code into individual classes
    -Exclude the console Projects from the build

---
+++ Project Euler Interface/Problems/P1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Euler_Interface.Problems
+{
+    class P1
+    {
+    }
+}
