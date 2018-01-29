b2530503c555f42eca8b87654354a5f918ea61f3
Sean Bostic <sean.bostic@gmail.com>
2018-01-29 00:01:31 -0500

    Problem combo box hard code
    Hard coded the selection to populate the question text box.

--- Project Euler Interface/Form1.Designer.cs
+++ Project Euler Interface/Form1.Designer.cs
@@ -67,6 +67,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(307, 21);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // progressBar1
             // 
